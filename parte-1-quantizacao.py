ca2e187468d7dabd91fdcdcb5a1b5955ad4e6931
Marcos Vinicius Wagner <57757951+marcosvwagner@users.noreply.github.com>
2026-08-06 18:59:01 +0000

    Unidades de medida variaveis

--- parte-1-quantizacao.py
+++ parte-1-quantizacao.py
@@ -5,15 +5,17 @@ import streamlit as st
 
 st.header("parte 1 - exercicio")
 
-dt = 1e-6 
-fa = 8.0     # Taxa de amostragem
-Ta = 1/fa    # Periodo de amostragem
+dt = 1e-6    # Passo de tempo continuo [s]
+fa = 8.0     # Taxa de amostragem [amostras/s]
+Ta = 1/fa    # Periodo de amostragem [s]
 L = 8        # Nº de nivels de quantização
-pa = 2.0      # Passo de quantizaçao
-t0 = 0.0     # Tempo inicial do intervalo de simulação
-tf = 1.0     # Tempo final de simulação
+delta = 2.0  # Passo de quantizaçao [v]
+t0 = 0.0     # Tempo inicial do intervalo de simulação [s]
+tf = 1.0     # Tempo final de simulação [s]
 
 # Mensagem
-ts = np.arange(t0,tf, step = dt)
-
+ts = np.arange(t0,tf, step=dt)
 x_t = 5.0 * np.sin(2*np.pi*ts)
+
+# Amostragem
+
